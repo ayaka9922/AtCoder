57cc61cae346549c0258ec2e1b72b52b25e163c8
山本純佳 <ayk.24a3y@gmail.com>
2020-10-06 23:59:15 +0900

    solved abc175

---
+++ contests/abc175/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int cnt = 0;
+  string s;
+  cin >> s;
+
+  for (int i = 0; i < 3; i++){
+    if (s[i] == 'R')
+    cnt++;
+  }
+  if (s == "RSR")
+    cnt == 1;
+
+  cout << cnt << endl;
+
+}
