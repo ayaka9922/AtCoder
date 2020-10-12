4623743b3369e139a96f30f85c44d748b2512717
Ayapi <ayk.24a3y@gmail.com>
2020-10-12 23:52:03 +0900

    arc105ab

--- contests/abc179/c.cpp
+++ contests/abc179/c.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 int main() {
-  int n, cnt = 0;
-  cin >> n;
 
-  for (int i = 1; i < n; i++) {
+int n, cnt = 0;
+cin >> n;
+
+for (int i = 1; i < n; i++) {
     for (int j = 1; j <= n/i; j++) {
         if ( i * j < n ) {
             cnt++;
         }
     }
-   }
+}
 
     cout << cnt << endl;
 }
