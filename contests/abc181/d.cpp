553c24c55ce3ea88cb6d377830034058363d2457
Ayapi <ayk.24a3y@gmail.com>
2020-11-03 16:57:24 +0900

    solved abc181d

--- contests/abc181/d.cpp
+++ contests/abc181/d.cpp
@@ -4,18 +4,58 @@ using ll = long long;
 
 int main() {
   string s;
-  istringstream s1;
   cin >> s;
 
-  int num;
-  s1 = istringstream(s);
-  s1 >> num;
+  //一桁のとき
+  if(s.length() == 1){
+		if(s[0] != '8'){
+			cout << "No" << endl;
+		}else{
+			cout << "Yes" << endl;
+		}
+		return 0;
+	}
 
-  if (num % 8 == 0) {
-    cout <<  "Yes" << endl;
-    return 0;
-  }
+  //二桁のとき
+	if(s.length() == 2){
+		int num = stoi(s);
+		if(num % 8 == 0){
+			cout << "Yes" << endl;
+		}else{
+			reverse(s.begin(), s.end());
+			num = stoi(s);
+			if (num % 8 == 0) {
+				cout << "Yes" << endl;
+			}else{
+				cout << "No" << endl;
+			}
+		}
+		return 0;
+	}
 
-  cout <<  "No" << endl;
-  return 0;
+	sort(s.begin(), s.end());
+	int cnt[10] = {0};
+	for(int i = 0; i < (int)s.length(); i++){
+		int num = s[i] - '0';
+		cnt[num]++;
+	}
+	cnt[0] = 0;
+	for (int i = 104; i < 1000; i += 8) {
+		int a, b, c;
+		int num = i;
+		int temp[10] = {0};
+		a = num % 10; num /= 10;
+		b = num % 10; num /= 10;
+		c = num % 10; num /= 10;
+		temp[a]++;
+    temp[b]++;
+    temp[c]++;
+		if (cnt[a] - temp[a] >= 0 && cnt[b] - temp[b] >= 0 && cnt[c] - temp[c] >= 0) {
+			cout << "Yes" << endl;
+			return 0;
+		}
+	}
+	cout << "No" << endl;
+
+	return 0;
 }
