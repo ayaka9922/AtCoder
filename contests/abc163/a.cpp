4ab9eee7ef72966e95876bda79122079a391231b
Ayapi <ayk.24a3y@gmail.com>
2020-11-11 23:57:55 +0900

    solved abc163abc

---
+++ contests/abc163/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  int r;
+  double pi = 3.14159265359;
+  cin >> r;
+
+  cout << 2 * r * pi << endl;
+
+  return 0;
+}
---
+++ contests/abc163/d.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n, k, mod = 1000000007;
+  int ten = 10000000;
+  cin >> n >> k;
+  set<int> cnt;
+  
+  for (int i = 0; i < n; i++){
+    cout << cnt.size() << endl;
+  }
+  return 0;
+}
