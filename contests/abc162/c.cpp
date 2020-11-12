3156cf627afdc98496e738081a04b60e194215cf
Ayapi <ayk.24a3y@gmail.com>
2020-11-12 23:53:43 +0900

    solved abc162

---
+++ contests/abc162/c.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  int k;
+  cin >> k;
+  ll ans = 0;
+  for (int i = 1; i <= n; i++) {
+    if (i % 3 != 0 && i % 5 != 0) {
+      ans += i;
+    }
+  }
+  cout << ans << endl;
+
+  return 0;
+}
--- contests/abc163/d.cpp
+++ contests/abc163/d.cpp
@@ -7,9 +7,10 @@ int main() {
   int ten = 10000000;
   cin >> n >> k;
   set<int> cnt;
-  
+
   for (int i = 0; i < n; i++){
-    cout << cnt.size() << endl;
+
   }
+  cout << cnt.size() << endl;
   return 0;
 }
