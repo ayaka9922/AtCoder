f4ef6c723a296f2e6627bf3fc1e65763878eaca9
ayaka9922 <ayk.24a3y@gmail.com>
2020-11-25 20:55:33 +0900

    solved abc156c

--- contests/abc156/b.cpp
+++ contests/abc156/b.cpp
@@ -5,11 +5,10 @@ using ll = long long;
 int main() {
   ll n, k;
   cin >> n >> k;
-  int keta = 1;
-  while (n != 1) {
+  ll keta = 1;
+  while (n >= k) {
     n = n / k;
     keta++;
-    cout << n << endl;
   }
   cout << keta << endl;
   return 0;
---
+++ contests/abc156/c.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> x(n);
+  for (int i = 0; i < n; i++) {
+    cin >> x[i];
+  }
+  int ans = 100000000;
+  for (int i = 1; i <= 100; i++) {
+    int tmp = 0;
+    for (int j = 0; j < n; j++) {
+      tmp += (x[j] - i) * (x[j] - i);
+    }
+    ans = min(ans, tmp);
+  }
+  cout << ans << endl;
+  return 0;
+}
