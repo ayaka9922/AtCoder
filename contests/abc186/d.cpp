563dbd27fc589d96379be248c351834a01284f44
ayaka9922 <ayk.24a3y@gmail.com>
2020-12-22 23:50:07 +0900

    solved abc186d

--- contests/abc186/d.cpp
+++ contests/abc186/d.cpp
@@ -6,21 +6,21 @@ int main() {
   int n;
   cin >> n;
   vector<int> a(n);
+  vector<ll> sum(n);
   for (int i = 0; i < n; i++) {
     cin >> a[i];
   }
   sort(a.begin(), a.end());
-  ll sum = 0;
-  int m = 1 - n;
-  for (int i = 0; i < n; i++) {
-    if (a[i] > 0) {
-      sum += a[i] * m;
-    } else {
-      a[i] = -a[i];
-      sum += a[i] * m;
-    }
-    m += 2;
+
+  sum[0] = a[0];
+  for (int i = 1; i < n; i++) {
+    sum[i] = sum[i - 1] + a[i];
   }
-  cout << sum;
+  ll ans = 0;
+  for (int i = 0; i < n - 1; i++) {
+    ans += sum[n - 1] - sum[i];
+    ans -= (ll)(n - i - 1) * a[i];
+  }
+  cout << ans << endl;
   return 0;
 }
